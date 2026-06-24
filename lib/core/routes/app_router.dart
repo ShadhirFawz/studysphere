1e59dcda61db63b51c10b51d8418bc88b38fd1a4
Shadhir Fawz <it22237118@my.sliit.lk>
2026-06-24 09:04:35 +0530

    feat: auth wrapper and providers with screens

--- lib/core/routes/app_router.dart
+++ lib/core/routes/app_router.dart
@@ -3,10 +3,11 @@ import 'package:go_router/go_router.dart';
 import '../../features/auth/presentation/screens/login_screen.dart';
 import '../../features/dashboard/presentation/screens/dashboard_screen.dart';
 import '../../features/auth/presentation/screens/register_screen.dart';
+import '../../features/auth/presentation/screens/auth_gate.dart';
 
 class AppRouter {
   static final router = GoRouter(
-    initialLocation: '/login',
+    initialLocation: '/',
     routes: [
       GoRoute(path: '/login', builder: (context, state) => const LoginScreen()),
       GoRoute(
@@ -17,6 +18,7 @@ class AppRouter {
         path: '/register',
         builder: (context, state) => const RegisterScreen(),
       ),
+      GoRoute(path: '/', builder: (context, state) => const AuthGate()),
     ],
   );
 }
--- lib/features/auth/presentation/screens/register_screen.dart
+++ lib/features/auth/presentation/screens/register_screen.dart
@@ -1,10 +1,47 @@
 import 'package:flutter/material.dart';
 
-class RegisterScreen extends StatelessWidget {
+class RegisterScreen extends StatefulWidget {
   const RegisterScreen({super.key});
 
   @override
+  State<RegisterScreen> createState() => _RegisterScreenState();
+}
+
+class _RegisterScreenState extends State<RegisterScreen> {
+  final nameController = TextEditingController();
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(body: Center(child: Text('Register Screen')));
+    return Scaffold(
+      appBar: AppBar(title: const Text("Create Account")),
+      body: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          children: [
+            TextField(
+              controller: nameController,
+              decoration: const InputDecoration(labelText: "Full Name"),
+            ),
+
+            TextField(
+              controller: emailController,
+              decoration: const InputDecoration(labelText: "Email"),
+            ),
+
+            TextField(
+              controller: passwordController,
+              obscureText: true,
+              decoration: const InputDecoration(labelText: "Password"),
+            ),
+
+            const SizedBox(height: 20),
+
+            ElevatedButton(onPressed: () {}, child: const Text("Register")),
+          ],
+        ),
+      ),
+    );
   }
 }
